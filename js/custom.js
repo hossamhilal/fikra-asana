1c863488fb284ba6b708cdbca16f9fef6e00e95e
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2021-01-11 09:28:10 +0300

    update

--- js/custom.js
+++ js/custom.js
@@ -43,7 +43,7 @@
                 // If we have a character, wrap it
                 if (substring != " ") {
                     newContent += '<span style="animation-delay:'+x+'s">' + substring + '</span>';
-                    x = x + 0.1;
+                    x = x + 0.19;
                 } else {
                     newContent += substring;
                 }
@@ -56,11 +56,42 @@
     setUpCharacters();
 
 
+    // Scroll To Top
     let wheight = $(document).height() / 2 - 500;
     $(window).scroll(function(){
         if ($(window).scrollTop() > wheight) $('.scrollTop').addClass('show');
         else $('.scrollTop').removeClass('show');
     });
+
+
+    // Side Dots
+    $('.sideMenu a').on('click', function(e) {
+        e.preventDefault();
+        $('.sideMenu a').removeClass('active');     
+        $(this).addClass('active');
+        $('html, body').animate({ 
+            scrollTop: $($(this).attr('href')).offset().top 
+        }, 1200, 'linear');
+    });
+
+    $(window).on('scroll', function(){
+        $('.sideMenu a').each(function () {
+            let currLink = $(this) ,
+                href = $(currLink.attr("href")) ,
+                itemPosition = href.position().top;
+            if ( $(window).scrollTop() >= itemPosition - 150 ) {
+                $('.sideMenu a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    });
+    
+    
+
+
 
     // Start Animation 
     AOS.init();
